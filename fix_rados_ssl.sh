1e68d930f105c38ea2e34f7faab3d451c1733c05
obutenko <obutenko@mirantis.com>
2016-02-26 12:31:24 +0200

    Add CONTROLLER_ID to fix_rados_ssl.sh

--- fix_rados_ssl.sh
+++ fix_rados_ssl.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -xe
 echo Fix RadosGW publicurl
-        swift_id=$(ssh node-${CONTROLLER_ID} ". openrc; openstack endpoint list |grep object-store | awk '{print\$2}'")
-        echo $swift_id
-        adminurl=$(ssh node-${CONTROLLER_ID} ". openrc; openstack endpoint show $swift_id |grep adminurl | awk '{print\$4}'")
-        echo $adminurl
-        internalurl=$(ssh node-${CONTROLLER_ID} ". openrc; openstack endpoint show $swift_id |grep internalurl | awk '{print\$4}'")
-        echo $internalurl
-        publicurl="https://public.fuel.local:8080/swift/v1"
-        echo $publicurl
-        region=$(ssh node-${CONTROLLER_ID} ". openrc; openstack endpoint show $swift_id |grep region | awk '{print\$4}'")
-        echo $region
-        ssh node-${CONTROLLER_ID} ". openrc; openstack endpoint delete $swift_id"
-        ssh node-${CONTROLLER_ID} ". openrc; openstack endpoint create swift --publicurl $publicurl --adminurl $adminurl --internalurl $internalurl --region $region"
+CONTROLLER_ID="$(fuel node | grep controller| awk '{print$1}'| head -1)"
+swift_id=$(ssh node-${CONTROLLER_ID} ". openrc; openstack endpoint list |grep object-store | awk '{print\$2}'")
+echo $swift_id
+adminurl=$(ssh node-${CONTROLLER_ID} ". openrc; openstack endpoint show $swift_id |grep adminurl | awk '{print\$4}'")
+echo $adminurl
+internalurl=$(ssh node-${CONTROLLER_ID} ". openrc; openstack endpoint show $swift_id |grep internalurl | awk '{print\$4}'")
+echo $internalurl
+publicurl="https://public.fuel.local:8080/swift/v1"
+echo $publicurl
+region=$(ssh node-${CONTROLLER_ID} ". openrc; openstack endpoint show $swift_id |grep region | awk '{print\$4}'")
+echo $region
+ssh node-${CONTROLLER_ID} ". openrc; openstack endpoint delete $swift_id"
+ssh node-${CONTROLLER_ID} ". openrc; openstack endpoint create swift --publicurl $publicurl --adminurl $adminurl --internalurl $internalurl --region $region"
