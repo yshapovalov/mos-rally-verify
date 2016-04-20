184163fbbc9c3b7b2473d35c0b96ebf935ae2fb3
obutenko <obutenko@mirantis.com>
2016-04-20 17:18:11 +0300

    refactor

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -xe
-CACERT_FILE_PATH="/var/lib/astute/haproxy/public_haproxy.pem"
+cp /root/openrc /home
 IS_TLS=$(source /root/openrc; keystone catalog --service identity 2>/dev/null | awk '/https/')
-i
+
 if [ "${IS_TLS}" ]; then
-    cp ${CACERT_FILE_PATH} /root
-    echo "export OS_CACERT='${CACERT_FILE_PATH}'" >> /root/openrc
+    cp /var/lib/astute/haproxy/public_haproxy.pem /home 
+    echo "export OS_CACERT='/home/rally/public_haproxy.pem'" >> /home/rally/openrc
 fi
 
 echo "sed -i 's|#swift_operator_role = Member|swift_operator_role = SwiftOperator|g' /etc/rally/rally.conf
-      source /root/openrc
-      git clone https://github.com/openstack/tempest.git /root/tempest
+      source /home/rally/openrc
+      git clone https://github.com/openstack/tempest.git /home/rally/tempest
       rally-manage db recreate
       rally deployment create --fromenv --name=tempest
       rally verify install --source /root/tempest
       rally verify genconfig
-      rally verify showconfig" > /root/install_tempest.sh
+      rally verify showconfig" > /home/install_tempest.sh
 
-chmod +x /root/install_tempest.sh
+chmod +x /home/install_tempest.sh
 
 apt-get install -y docker.io
 docker pull rallyforge/rally:0.4.0
 image_id=$(docker images | grep 0.4.0| awk '{print $3}')
-docker run --net host -v /root/:/root -tid -u root $image_id
+docker run --net host -v /home/:/home/rally -tid -u root $image_id
 docker_id=$(docker ps | grep $image_id | awk '{print $1}')
 docker exec -ti $docker_id bash
