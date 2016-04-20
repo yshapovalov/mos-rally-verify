ebfe63ca2b03a2fdd723289ab87d7d1010f21cfc
obutenko <obutenko@mirantis.com>
2016-04-20 17:43:00 +0300

    add fix for heat

--- prepare_env.sh
+++ prepare_env.sh
@@ -8,13 +8,20 @@ if [ "${IS_TLS}" ]; then
 fi
 
 echo "sed -i 's|#swift_operator_role = Member|swift_operator_role = SwiftOperator|g' /etc/rally/rally.conf
+      apt-get install -y vim
       source /home/rally/openrc
       git clone https://github.com/openstack/tempest.git /home/rally/tempest
       rally-manage db recreate
       rally deployment create --fromenv --name=tempest
       rally verify install --source /home/rally/tempest
       rally verify genconfig
-      rally verify showconfig" > /home/install_tempest.sh
+      tconf=$(sudo find / -name tempest.conf)
+      sed -i "79i max_template_size = 5440000" $tconf 
+      sed -i "80i max_resources_per_stack = 20000" $tconf 
+      sed -i "81i max_json_body_size = 10880000" $tconf
+      echo "[volume]" >> $tconf
+      echo "build_timeout = 300" >> $tconf
+      verify showconfig" > /home/install_tempest.sh
 
 chmod +x /home/install_tempest.sh
 
