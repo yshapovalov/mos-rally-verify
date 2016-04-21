f00fa8a8e2f9817dc75ce773c97df6ae3a2d977f
obutenko <obutenko@mirantis.com>
2016-04-21 16:03:26 +0300

    Fix bug

--- prepare_env.sh
+++ prepare_env.sh
@@ -28,7 +28,7 @@ apt-get install -y docker.io
 docker pull rallyforge/rally:0.4.0
 image_id=$(docker images | grep 0.4.0| awk '{print $3}')
 docker run --net host -v /home/:/home/rally -tid -u root $image_id
-docker_id=$(docker ps | grep $image_id | awk '{print $1}')
+docker_id=$(docker ps | grep $image_id | awk '{print $1}'| head -1)
 
 docker exec -ti $docker_id bash -c "./install-tempest"
 tconf=$(find /home -name tempest.conf)
