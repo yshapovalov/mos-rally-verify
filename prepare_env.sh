78eeec7117e4ac4c0d5b317616744e33687e120f
Oleksii Butenko <obutenko@mirantis.com>
2016-06-02 13:43:59 +0300

    refactor

--- prepare_env.sh
+++ prepare_env.sh
@@ -2,7 +2,7 @@
 
 function prepare {
     cp /root/openrc /home
-    cp run_debug.sh /home
+    cp -r helpers/* /home
     V2_FIX=$(cat /home/openrc |grep v2.0| wc -l)
     if [ ${V2_FIX} == '0' ]; then
         sed -i 's|:5000|:5000/v2.0|g' /home/openrc
@@ -15,30 +15,19 @@ function prepare {
         cp /var/lib/astute/haproxy/public_haproxy.pem /home 
         echo "export OS_CACERT='/home/rally/public_haproxy.pem'" >> /home/openrc
     fi
-
-    echo "sed -i 's|#swift_operator_role = Member|swift_operator_role = SwiftOperator|g' /etc/rally/rally.conf
-          source /home/rally/openrc
-	  git clone https://github.com/openstack/tempest.git /home/rally/tempest
-          cd /home/rally/tempest
-          git checkout 63cb9a3718f394c9da8e0cc04b170ca2a8196ec2
-	  rally-manage db recreate
-	  rally deployment create --fromenv --name=tempest
-	  rally verify install --source /home/rally/tempest
-	  rally verify genconfig" >> /home/install-tempest
-    chmod +x /home/install-tempest
 }
 
 function install_docker_and_run {
     apt-get install -y docker.io
     apt-get install -y cgroup-bin
-    docker pull rallyforge/rally:0.4.0
-    image_id=$(docker images | grep 0.4.0| awk '{print $3}')
+    docker pull rallyforge/rally:latest
+    image_id=$(docker images | grep latest| awk '{print $3}')
     docker run --net host -v /home/:/home/rally -tid -u root $image_id
     docker_id=$(docker ps | grep $image_id | awk '{print $1}'| head -1)
 }
 
 function configure_tempest {
-    docker exec -ti $docker_id bash -c "./install-tempest"
+    docker exec -ti $docker_id bash -c "./install-tempest.sh"
     docker exec -ti $docker_id bash -c "apt-get install -y vim"
     tconf=$(find /home -name tempest.conf)
     storage_protocol="iSCSI"
