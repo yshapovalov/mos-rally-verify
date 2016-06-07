b73667744c94e4c6bed6e87c4bf12a5606d9d488
Yevgeniy <yshapovalov@mirantis.com>
2016-06-07 15:03:17 +0300

    tempest version for MOS 8.0

--- install_tempest.sh
+++ install_tempest.sh
@@ -3,5 +3,5 @@ sed -i 's|#swift_operator_role = Member|swift_operator_role = SwiftOperator|g' /
 source /home/rally/openrc
 rally-manage db recreate
 rally deployment create --fromenv --name=tempest 
-rally verify install --version 63cb9a3718f394c9da8e0cc04b170ca2a8196ec2
+rally verify install --version 55511d98f1f525edec476fe638ac13366ba5b03b
 rally verify genconfig
