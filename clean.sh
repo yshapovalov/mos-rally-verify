d3e3b48c529c21160122a539a45039f743fe13f0
yshapovalov <yshapovalov@mirantis.com>
2016-08-01 11:31:08 +0300

    Fix clean.sh for rally 0.5.0

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
-image_id=$(docker images | grep latest| awk '{print $3}')
+image_id=$(docker images | grep 0.5.0 | awk '{print $3}')
 docker_list=$(docker ps -a| grep $image_id |awk '{print$1}')
 docker rm -f $docker_list
 rm -rf /home/.rally
